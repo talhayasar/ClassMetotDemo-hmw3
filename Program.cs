89852cc59f6814a43d3362c2f722301c35b1a5f4
Muhammed Talha YAŞAR <talhay526@gmail.com>
2021-02-03 20:18:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassMetotDemo_hmw3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
